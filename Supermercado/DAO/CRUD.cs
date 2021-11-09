9e2546788f4636943b15d0f89b8a0250440134c7
RANDALL SANDOVAL CHAVARRIA <randall.sandoval.chavarria@est.una.ac.cr>
2021-11-08 18:32:49 -0600

    fix connectionString

--- Supermercado/DAO/CRUD.cs
+++ Supermercado/DAO/CRUD.cs
@@ -10,15 +10,25 @@ namespace Supermercado.DAO
     {
         public void insertarProducto(int _idArea, long _ean, string _descripcion, int _precio, int _cantidad)
         {
-            OracleConnection connection = GetConnection();
-            string query = string.Format("INSERT INTO producto (pk_idproducto, fk_idarea, ean, descripcion, precio, cantidad) VALUES (producto_sq.nextval,'{0}','{1}','{2}','{3}','{4}');", _idArea, _ean, _descripcion, _precio, _cantidad);
-            OracleCommand accion = new OracleCommand(query, connection);
-            accion.CommandTimeout = 60;
-            connection.Open();
-            //Ejecuta el query
-            accion.ExecuteReader();
-            //Cierra conexion
-            connection.Close();
+            try
+            {
+                OracleConnection connection = GetConnection();
+                string query = string.Format("INSERT INTO producto (pk_idproducto, fk_idarea, ean, descripcion, precio, cantidad) VALUES (producto_sq.nextval,'{0}','{1}','{2}','{3}','{4}');", _idArea, _ean, _descripcion, _precio, _cantidad);
+                query = "INSERT INTO super.producto (pk_idproducto, fk_idarea, ean, descripcion, precio, cantidad) VALUES (18, 1, 3693693693693,'aceite',1800, 30)";
+                OracleCommand accion = new OracleCommand(query, connection);
+                accion.CommandTimeout = 60;
+                connection.Open();
+                //Ejecuta el query
+                accion.ExecuteNonQuery();
+                //Cierra conexion
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+            
         }
     }
 }
--- Supermercado/DAO/ConexionBD.cs
+++ Supermercado/DAO/ConexionBD.cs
@@ -1,13 +1,19 @@
 ﻿using System;
-using Oracle.ManagedDataAccess.Client;
+using System.Data.OracleClient;
+
 
 namespace Supermercado.DAO
 {
     public class ConexionBD
     {
-        protected OracleConnection GetConnection()
+        private static string connectionString = @"Data Source=(DESCRIPTION=
+    (ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=localhost )(PORT=1521)))
+    (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XE))); User ID=SUPER; PASSWORD=superUsuario;";
+        
+        protected Oracle.ManagedDataAccess.Client.OracleConnection GetConnection()
         {
-            return new OracleConnection("DATA SOURCE = XE; PASSWORD = superUsuario; USER ID = SUPER;");
+            
+            return new Oracle.ManagedDataAccess.Client.OracleConnection(connectionString);
         }
     }
 }
